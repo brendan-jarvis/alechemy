1caac02fca57fb1a1666f815b5a093964c7959e9
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-04-20 19:06:58 +1200

    Inter font family

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,9 @@ export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+    },
   },
   plugins: [],
 } satisfies Config;
