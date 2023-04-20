84ab8749990f9c4bad3921d25b45e2675503259b
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-04-20 18:59:54 +1200

    Recipe router

---
+++ src/server/api/routers/recipes.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+export const recipesRouter = createTRPCRouter({
+  getAll: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.recipe.findMany();
+  }),
+});
