aa7972d87cf9d1ea8170e2eee80d4b55a0235d9c
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-04-21 18:08:11 +1200

    Async with limit

--- src/server/api/routers/recipes.ts
+++ src/server/api/routers/recipes.ts
@@ -1,9 +1,15 @@
+import { clerkClient } from "@clerk/nextjs/server";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const recipesRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.recipe.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const recipes = await ctx.prisma.recipe.findMany({
+      take: 10,
+      orderBy: { createdAt: "desc" },
+    });
+
+    return recipes;
   }),
 });
