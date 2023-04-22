c76e2f651e1a87a4369164e1eb61e48da997b234
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-04-23 01:20:00 +1200

    Add author to Recipe Query

--- src/server/api/routers/recipes.ts
+++ src/server/api/routers/recipes.ts
@@ -1,6 +1,49 @@
+import clerkClient from "@clerk/clerk-sdk-node";
 import { z } from "zod";
-
+import type { User } from "@clerk/nextjs/dist/api";
+import type { Recipe } from "@prisma/client";
+import { TRPCError } from "@trpc/server";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+export const filterUserForClient = (user: User) => {
+  return {
+    id: user.id,
+    username: user.username,
+    profileImageUrl: user.profileImageUrl,
+  };
+};
+
+const addUserDataToRecipes = async (recipes: Recipe[]) => {
+  const userId = recipes.map((recipe) => recipe.authorId);
+  const users = (
+    await clerkClient.users.getUserList({
+      userId: userId,
+      limit: 100,
+    })
+  ).map(filterUserForClient);
+
+  return recipes.map((recipe) => {
+    const author = users.find((user) => user.id === recipe.authorId);
+
+    if (!author) {
+      console.error("AUTHOR NOT FOUND", recipe);
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: `Author for recipe not found. RECIPE ID: ${recipe.id}, USER ID: ${recipe.authorId}`,
+      });
+    }
+    if (!author.username) {
+      author.username = "Brew! User";
+    }
+    return {
+      recipe,
+      author: {
+        ...author,
+        username: author.username ?? "Unknown Brew! User",
+      },
+    };
+  });
+};
 
 export const recipesRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
@@ -9,6 +52,6 @@ export const recipesRouter = createTRPCRouter({
       orderBy: { createdAt: "desc" },
     });
 
-    return recipes;
+    return addUserDataToRecipes(recipes);
   }),
 });
