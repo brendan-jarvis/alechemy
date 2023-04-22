331aaf9be0450c886940196a90259f3cb9ac3190
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-04-23 01:19:36 +1200

    Random card images, add author to card

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { SignInButton, useUser } from "@clerk/nextjs";
 import Image from "next/image";
 import { Nav } from "~/components/Nav";
 import { Footer } from "~/components/Footer";
+import { AddRecipe } from "~/components/AddRecipe";
 
 import { api } from "~/utils/api";
 
@@ -30,52 +31,57 @@ const Home: NextPage = () => {
       {user.isSignedIn ? (
         <>
           <Nav />
-          <div
-            className="hero min-h-screen"
-            style={{
-              backgroundImage: `url("/images/claude-piche-EHbtjmz7hvw-unsplash.jpg")`,
-            }}
-          >
-            <div className="hero-overlay bg-opacity-60"></div>
-            <div className="hero-content text-center text-neutral-content">
-              <div className="max-w-md">
-                <h1 className="mb-5 text-5xl font-bold">Latest recipes</h1>
-                {data?.map((recipe) => (
-                  <div
-                    key={recipe.id}
-                    className="card w-96 bg-neutral-focus shadow-xl"
-                  >
-                    <figure>
-                      <Image
-                        src={
-                          recipe.image
-                            ? recipe.image
-                            : "/images/lutz-wernitz-pcW5bR7gSJ4-unsplash.jpg"
-                        }
-                        width={500}
-                        height={500}
-                        alt={`${recipe.title} ${recipe.description}`}
-                      />
-                    </figure>
-                    <div className="card-body">
-                      <h2 className="card-title">
-                        {recipe.title}
-                        <div className="badge-secondary badge">NEW</div>
-                      </h2>
-                      <p>{recipe.description}</p>
-                      <div className="card-actions justify-end">
-                        <div className="badge-outline badge text-info">
-                          {recipe.content.beerjson.recipes[0].style.name.toUpperCase()}
-                        </div>
-                        <div className="badge-outline badge text-success">
-                          {recipe.content.beerjson.recipes[0].type.toUpperCase()}
-                        </div>
+          <div className="max-w-md">
+            <h1 className="m-4 text-center font-serif text-5xl font-bold text-neutral">
+              Latest recipes
+            </h1>
+
+            <AddRecipe />
+
+            {data?.map((props) => {
+              const { recipe, author } = props;
+
+              return (
+                <div
+                  key={recipe.id}
+                  className="card bg-neutral text-base-200 shadow-xl hover:bg-neutral-focus"
+                >
+                  <figure>
+                    <Image
+                      src={
+                        recipe.image
+                          ? recipe.image
+                          : [
+                              "/images/markus-spiske-qn5iDwvOZgo-unsplash.jpg",
+                              "/images/lutz-wernitz-pcW5bR7gSJ4-unsplash.jpg",
+                              "/images/meritt-thomas-OGTEP0LyYNk-unsplash.jpg",
+                            ][Math.floor(Math.random() * 3)]
+                      }
+                      className={recipe.image ? "" : "opacity-50"}
+                      width={500}
+                      height={500}
+                      alt={`${recipe.title} ${recipe.description}`}
+                    />
+                  </figure>
+                  <div className="card-body">
+                    <h2 className="card-title">
+                      {recipe.title}
+                      <div className="badge-secondary badge">NEW</div>
+                    </h2>
+                    <p className="text-sm italic">By {author?.username}</p>
+                    <p>{recipe.description}</p>
+                    <div className="card-actions justify-end">
+                      <div className="badge-outline badge text-sm text-info">
+                        {recipe.content.beerjson.recipes[0].style.name.toUpperCase()}
+                      </div>
+                      <div className="badge-outline badge text-sm text-success">
+                        {recipe.content.beerjson.recipes[0].type.toUpperCase()}
                       </div>
                     </div>
                   </div>
-                ))}
-              </div>
-            </div>
+                </div>
+              );
+            })}
           </div>
           <Footer />
         </>
@@ -90,7 +96,10 @@ const Home: NextPage = () => {
           <div className="hero-content text-center text-neutral-content">
             <div className="max-w-md">
               <h1 className="mb-5 text-5xl font-bold">Brew!</h1>
-              <SignInButton className="btn-primary btn" />
+              {/* btn-primary btn text-xl text-neutral  */}
+              <div className="btn-primary btn rounded bg-primary font-bold text-neutral hover:btn-success">
+                <SignInButton />
+              </div>
             </div>
           </div>
         </div>
