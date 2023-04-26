b0b57e52b09c749498337fd7a823adb5dbf0cb37
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-04-26 19:07:12 +1200

    Change default image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -40,50 +40,42 @@ const Home: NextPage = () => {
 
             <AddRecipe />
 
-            {data?.map((props) => {
-              const { recipe, author } = props;
-
-              return (
-                <div
-                  key={recipe.id}
-                  className="card bg-neutral text-base-200 shadow-xl hover:bg-neutral-focus"
-                >
-                  <figure>
-                    <Image
-                      src={
-                        recipe.image
-                          ? recipe.image
-                          : [
-                              "/images/markus-spiske-qn5iDwvOZgo-unsplash.jpg",
-                              "/images/lutz-wernitz-pcW5bR7gSJ4-unsplash.jpg",
-                              "/images/meritt-thomas-OGTEP0LyYNk-unsplash.jpg",
-                            ][Math.floor(Math.random() * 3)]
-                      }
-                      className={recipe.image ? "" : "opacity-50"}
-                      width={500}
-                      height={500}
-                      alt={`${recipe.title} ${recipe.description}`}
-                    />
-                  </figure>
-                  <div className="card-body">
-                    <h2 className="card-title">
-                      {recipe.title}
-                      <div className="badge-secondary badge">NEW</div>
-                    </h2>
-                    <p className="text-sm italic">By {author?.username}</p>
-                    <p>{recipe.description}</p>
-                    <div className="card-actions justify-end">
-                      <div className="badge-outline badge text-sm text-info">
-                        {recipe.content.beerjson.recipes[0].style.name.toUpperCase()}
-                      </div>
-                      <div className="badge-outline badge text-sm text-success">
-                        {recipe.content.beerjson.recipes[0].type.toUpperCase()}
-                      </div>
+            {[...data, ...data]?.map(({ recipe, author }) => (
+              <div
+                key={recipe.id}
+                className="card bg-neutral text-base-200 shadow-xl hover:bg-neutral-focus"
+              >
+                <figure>
+                  <Image
+                    src={
+                      recipe.image
+                        ? recipe.image
+                        : "/images/jon-parry-C8eSYwQkwHw-unsplash.jpg"
+                    }
+                    className={recipe.image ? "" : "opacity-50"}
+                    width={500}
+                    height={500}
+                    alt={`${recipe.title} ${recipe.description}`}
+                  />
+                </figure>
+                <div className="card-body">
+                  <h2 className="card-title">
+                    {recipe.title}
+                    <div className="badge-secondary badge">NEW</div>
+                  </h2>
+                  <p className="text-sm italic">By {author?.username}</p>
+                  <p>{recipe.description}</p>
+                  <div className="card-actions justify-end">
+                    <div className="badge-outline badge text-sm text-info">
+                      {recipe.content.beerjson.recipes[0].style.name.toUpperCase()}
+                    </div>
+                    <div className="badge-outline badge text-sm text-success">
+                      {recipe.content.beerjson.recipes[0].type.toUpperCase()}
                     </div>
                   </div>
                 </div>
-              );
-            })}
+              </div>
+            ))}
           </div>
           <Footer />
         </>
