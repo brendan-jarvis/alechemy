41dee100a8e34b802d895438f9c6a7ce2a4acdc7
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-04-26 20:01:53 +1200

    RecipeView component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,45 @@ import { AddRecipe } from "~/components/AddRecipe";
 
 import { api } from "~/utils/api";
 
+const RecipeView = ({ recipe, author }) => {
+  return (
+    <div
+      key={recipe.id}
+      className="card bg-neutral text-base-200 shadow-xl hover:bg-neutral-focus"
+    >
+      <figure>
+        <Image
+          src={
+            recipe.image
+              ? recipe.image
+              : "/images/jon-parry-C8eSYwQkwHw-unsplash.jpg"
+          }
+          className={recipe.image ? "" : "opacity-50"}
+          width={500}
+          height={500}
+          alt={`${recipe.title} ${recipe.description}`}
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">
+          {recipe.title}
+          <div className="badge-secondary badge">NEW</div>
+        </h2>
+        <p className="text-sm italic">By {author?.username}</p>
+        <p>{recipe.description}</p>
+        <div className="card-actions justify-end">
+          <div className="badge-outline badge text-sm text-info">
+            {recipe.content?.beerjson.recipes[0].style.name.toUpperCase()}
+          </div>
+          <div className="badge-outline badge text-sm text-success">
+            {recipe.content?.beerjson.recipes[0].type.toUpperCase()}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const Home: NextPage = () => {
   const user = useUser();
   const { data, isLoading } = api.recipes.getAll.useQuery();
@@ -41,40 +80,7 @@ const Home: NextPage = () => {
             <AddRecipe />
 
             {[...data, ...data]?.map(({ recipe, author }) => (
-              <div
-                key={recipe.id}
-                className="card bg-neutral text-base-200 shadow-xl hover:bg-neutral-focus"
-              >
-                <figure>
-                  <Image
-                    src={
-                      recipe.image
-                        ? recipe.image
-                        : "/images/jon-parry-C8eSYwQkwHw-unsplash.jpg"
-                    }
-                    className={recipe.image ? "" : "opacity-50"}
-                    width={500}
-                    height={500}
-                    alt={`${recipe.title} ${recipe.description}`}
-                  />
-                </figure>
-                <div className="card-body">
-                  <h2 className="card-title">
-                    {recipe.title}
-                    <div className="badge-secondary badge">NEW</div>
-                  </h2>
-                  <p className="text-sm italic">By {author?.username}</p>
-                  <p>{recipe.description}</p>
-                  <div className="card-actions justify-end">
-                    <div className="badge-outline badge text-sm text-info">
-                      {recipe.content?.beerjson.recipes[0].style.name.toUpperCase()}
-                    </div>
-                    <div className="badge-outline badge text-sm text-success">
-                      {recipe.content?.beerjson.recipes[0].type.toUpperCase()}
-                    </div>
-                  </div>
-                </div>
-              </div>
+              <RecipeView key={recipe.id} recipe={recipe} author={author} />
             ))}
           </div>
           <Footer />
