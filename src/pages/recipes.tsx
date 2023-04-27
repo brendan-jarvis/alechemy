4217eb3749b274eedc67bcf87d34c45c310bba63
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-04-27 18:12:19 +1200

    Add Nav and Header to isLoading !data states

--- src/pages/recipes.tsx
+++ src/pages/recipes.tsx
@@ -8,25 +8,38 @@ import { api } from "~/utils/api";
 const Recipes: NextPage = () => {
   const { data, isLoading } = api.recipes.getAll.useQuery();
 
+  const Header = () => (
+    <h1 className="m-4 text-center font-serif text-5xl font-bold text-neutral">
+      Latest recipes
+    </h1>
+  );
+
   if (isLoading)
     return (
-      <div className="bg-blue-100 p-4 text-secondary-focus">Loading...</div>
+      <>
+        <Nav />
+        <Header />
+        <div className="bg-blue-100 p-4 text-secondary-focus">Loading...</div>
+        <Footer />
+      </>
     );
 
   if (!data)
     return (
-      <div className="bg-red-100 p-4 text-error">Something went wrong!</div>
+      <>
+        <Nav />
+        <Header />
+        <div className="bg-red-100 p-4 text-error">Something went wrong!</div>
+        <Footer />
+      </>
     );
 
   return (
     <>
       <Nav />
 
-      <div className="max-w-md">
-        <h1 className="m-4 text-center font-serif text-5xl font-bold text-neutral">
-          Latest recipes
-        </h1>
-
+      <Header />
+      <div className="flex flex-row flex-wrap justify-center gap-4 p-4">
         {data?.map((props) => (
           <RecipeView key={props.recipe.id} {...props} />
         ))}
